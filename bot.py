7d12bf9b33cbf3ce28abd533acc9fbd52ca5cf1e
Sizzl <sizzl@utassault.net>
2020-06-29 00:20:50 +0100

    Returned command_prefix to default

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ log.addHandler(handler)
 
 help_attrs = dict(hidden=True)
 bot = commands.Bot(
-        command_prefix=['^','?'],
+        command_prefix=['!','.'],
         description=description,
         pm_help=None,
         help_attrs=help_attrs)
