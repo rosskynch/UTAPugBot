f775dece54b571035ba89e7989a8a9081f95542c
Ross Kynch <rkynch@gmail.com>
2020-07-28 00:39:04 +0100

    Updated info class to make use of the admin check added in the previous commits.
    Up-versions to v1.0.0.0 and added github link.

--- cogs/info.py
+++ cogs/info.py
@@ -1,10 +1,9 @@
 import asyncio
 from discord.ext import commands
 import discord
+from cogs import admin
 
-# Note that the command 'invite' checks if it came from a particular server
-
-VERSION = '0.1'
+VERSION = '1.0.0.0'
 URL = 'www.utassault.net'
 UTASSAULT = 250997389308067841
 
@@ -18,14 +17,14 @@ class Info(commands.Cog):
         result = ['**__About Me:__**']
         result.append('- Version: {}'.format(VERSION))
         result.append('- Author: uZi')
-        result.append('- Library: discord.py')
+        result.append('- GitHub: https://github.com/rosskynch/UTAPugBot')
+        result.append('- Library: discord.py')        
         await ctx.send('\n'.join(result))
 
     @commands.command()
     async def website(self, ctx):
         await ctx.send(URL)
 
-    
     @commands.command()
     @commands.guild_only()
     async def invite(self, ctx):
@@ -33,6 +32,7 @@ class Info(commands.Cog):
 
         Bot must have proper permissions to get this information
         """
+        # Note that the command 'invite' checks if it came from a particular server
         if ctx.message.guild.id == UTASSAULT:
             # Stuff for UTAssault discord server
             # Perma-invite I created.
@@ -51,7 +51,7 @@ class Info(commands.Cog):
                     await ctx.send('No invites available')
 
     @commands.command()
-    @commands.has_permissions(manage_guild=True)
+    @commands.check(commands.check(admin.hasManagerRole_Check))
     async def checkpermissions(self, ctx):
         """Checks user permissions are correct to use elevated commands"""
         await ctx.send('You have permissions.')
