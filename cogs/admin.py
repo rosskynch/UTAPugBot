74f19d9b7d9e780e4a645018dc6ab53063f781c4
Ross Kynch <rkynch@gmail.com>
2020-07-28 00:01:52 +0100

    Added role handling for admin commands. Also made the role persistent by storing it in the config file (same one used for the maplist). This is stored in the servers folder - might consider moving this to a "config" folder or something, but it's fine for now.

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,6 +1,14 @@
 import discord
+import json
 from discord.ext import commands, tasks
 
+DEFAULT_CONFIG_FILE = 'servers/config.json'
+DEFAULT_MANAGER_ROLE = 'PugBotManager'
+
+#########################################################################################
+# Utilities.
+#########################################################################################
+def hasManagerRole_Check(ctx): return ctx.bot.get_cog('Admin').hasManagerRole(ctx)
 
 #########################################################################################
 # Admin cog class.
@@ -8,15 +16,74 @@ from discord.ext import commands, tasks
 class Admin(commands.Cog):
     """Admin-only commands that make the bot dynamic."""
 
-    def __init__(self, bot):
+    def __init__(self, bot, configFile=DEFAULT_CONFIG_FILE):
         self.bot = bot
+        self.managerRole = DEFAULT_MANAGER_ROLE
+        self.configFile = configFile
+
+        self.loadConfig(self.configFile)
+
+#########################################################################################
+# Utilities.
+#########################################################################################
+    def loadConfig(self, configFile):
+        with open(configFile) as f:
+            info = json.load(f)
+            self.managerRole = info['admin']['managerrole'] # role should be stored as the name.
+            print("Loaded manager role: {0}".format(self.managerRole))
+            f.close()
+        return True
+
+    # Update config (currently only maplist is being saved)
+    def saveConfig(self, configFile):
+        with open(configFile) as f:
+            info = json.load(f)
+            info['admin']['managerrole'] = self.managerRole
+        with open(configFile,'w') as f:
+            json.dump(info,f, indent=4)
+        return True
+
+    def hasManagerRole(self, ctx):
+        # Always allow if the author is a server admin:
+        if commands.check(commands.has_permissions(manage_guild=True)):
+            return True
+        # Otherwise check roles:
+        # Uses the name of the role to find it. An alternative is to use the whole role or id,
+        # but this makes setting it manually in the config file a bit more of a pain.
+        # Note also handle mention so if the role is set by mentioning the role, rather than
+        # just by name, it will still work.        
+        for i in ctx.author.roles:
+            if self.managerRole == i.name or self.managerRole == i.mention:
+                return True
+        return False
+
+#########################################################################################
+# Commands
+#########################################################################################
+    @commands.command(hidden=True, invoke_without_command=True)
+    @commands.is_owner()
+    async def setmanagerrole(self, ctx, role):
+        """Sets the role which the bot will accept admin commands from"""
+        if role in ctx.guild.roles:
+            await ctx.send("Previous role was: **{0}**. New role is: **{1}**.".format(self.managerRole, role))
+            self.managerRole = role
+            self.saveConfig(self.configFile)
+        else:
+            await ctx.send("**{0}** role is not available or not formatted properly. Role is still set to **{1}**".format(role, self.managerRole))
 
     @commands.command(hidden=True, invoke_without_command=True)
     @commands.is_owner()
     async def shutdown(self, ctx):
+        """Shuts the bot down. It must be restarted manually."""
         print("Shutting down.")
         await ctx.send("Shutting down.")
         await ctx.bot.logout()
+
+    @commands.command(hidden=True, invoke_without_command=True)
+    @commands.is_owner()
+    async def getmanagerrole(self, ctx):
+        """Gets the current role which the bot will accept admin commands from"""
+        await ctx.send("Current manager role is set to: **{0}**".format(self.managerRole))
 
     @commands.command(hidden=True, invoke_without_command=True)
     @commands.is_owner()
@@ -51,5 +118,23 @@ class Admin(commands.Cog):
         else:
             await ctx.send('\N{OK HAND SIGN}')
 
+    @commands.group(name='reload', hidden=True, invoke_without_command=True)
+    @commands.is_owner()
+    async def _reload(self, ctx, *, module):
+        """Reloads a module."""
+        try:
+            self.bot.reload_extension(module)
+        except commands.ExtensionError as e:
+            await ctx.send(f'{e.__class__.__name__}: {e}')
+        else:
+            await ctx.send('\N{OK HAND SIGN}')
+
+    @commands.command(hidden=True, invoke_without_command=True)
+    @commands.is_owner()
+    async def shutdown(self, ctx):
+        print("Shutting down.")
+        await ctx.send("Shutting down.")
+        await ctx.bot.logout()
+
 def setup(bot):
-    bot.add_cog(Admin(bot))
+    bot.add_cog(Admin(bot, DEFAULT_CONFIG_FILE))
